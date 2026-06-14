2cdfc3f9fd6704aba1e15ae2345e4fa52c2092fb
Shivam <114076309+shivamdotdevx@users.noreply.github.com>
2026-06-14 21:26:53 +0530

    update two

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ function App() {
   const [count, setCount] = useState(0)
       
   return (
-    <>
+    <>  
+
+      
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
